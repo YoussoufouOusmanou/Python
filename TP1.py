da6b98e6b6f573a37541df498d07573fe0c77197
Ousmanou YOUSSOUFOU <39488510+YoussoufouOusmanou@users.noreply.github.com>
2022-03-01 16:26:30 +0100

    Update TP1.py

--- TP1.py
+++ TP1.py
@@ -7,3 +7,15 @@ def loulou():
          print(i, "est modulo 3 riri")
         elif (i % 5) == 0:
          print(i, "est modulo 5 fifi")
+       
+def lis (tab):
+    i = 1
+    j = 0
+    for i in tab:
+        i = i*2
+        tab[j]=i
+        j=j+1
+    return print(tab)
+    
+loulou()
+lis([1,2,4,6])
