ef0db29ec0c46e0c4d2719f9efa1f0a01f5ce2e5
Ousmanou YOUSSOUFOU <39488510+YoussoufouOusmanou@users.noreply.github.com>
2022-03-01 16:00:48 +0100

    Create TP1.py

---
+++ TP1.py
@@ -0,0 +1,9 @@
+def loulou():
+    print("welcome")
+    for i in range (1, 100) :
+        if (i % 3) ==0 and (i % 5) == 0:
+         print(i, "est modulo 3 et 5  loulou")
+        elif (i % 3 ) == 0: 
+         print(i, "est modulo 3 riri")
+        elif (i % 5) == 0:
+         print(i, "est modulo 5 fifi")
